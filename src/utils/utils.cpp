7f4a75c6069095116bd0e9e52d6948cb2afb9319
CabineTsv <jimberthubert@gmail.com>
2026-08-09 20:32:20 +0800

    Add files via upload

---
+++ src/utils/utils.cpp
@@ -0,0 +1,68 @@
+#include "utils.hpp"
+
+using namespace geode::prelude;
+
+std::time_t Utils::getFileCreationTime(const std::filesystem::path& path) {
+    if (!std::filesystem::exists(path)) return 0;
+    
+    auto ftime = std::filesystem::last_write_time(path);
+    
+    using namespace std::chrono;
+    using file_clock = decltype(ftime)::clock;
+    
+    const auto file_time = time_point_cast<system_clock::duration>(
+        ftime - file_clock::now() + system_clock::now()
+    );
+    
+    return system_clock::to_time_t(file_time);
+}
+
+std::string Utils::formatTime(std::time_t time) {
+    auto tm = geode::localtime(time);
+
+    return fmt::format("{:%Y-%m-%d %H:%M:%S}", tm);
+}
+
+std::string Utils::getTexture() {
+    cocos2d::ccColor3B color = Mod::get()->getSettingValue<cocos2d::ccColor3B>("background_color");
+    std::string texture = color == ccc3(51, 68, 153) ? "GJ_square02.png" : "GJ_square06.png";
+    
+    return texture;
+}
+
+void Utils::setBackgroundColor(geode::NineSlice* bg) {
+    cocos2d::ccColor3B color = Mod::get()->getSettingValue<cocos2d::ccColor3B>("background_color");
+    
+    if (color == ccc3(51, 68, 153)) {
+        color = ccc3(255, 255, 255);
+        bg->setColor(color);
+    }
+}
+
+PauseLayer* Utils::getPauseLayer() {
+    if (auto scene = CCScene::get()) 
+        return scene->getChildByType<PauseLayer>(0);
+    
+    return nullptr;
+}
+
+void Utils::setTextInputEnabled(
+    TextInput* input,
+    bool enabled,
+    GLubyte enabledBg,
+    GLubyte disabledBg,
+    GLubyte enabledText,
+    GLubyte disabledText) 
+{
+    if (!input) return;
+
+    input->getBGSprite()->setOpacity(enabled ? enabledBg : disabledBg);
+    input->setEnabled(enabled);
+    input->getInputNode()->m_textLabel->setOpacity(enabled ? enabledText : disabledText);
+
+    if (!enabled) {
+        input->getInputNode()->detachWithIME();
+        input->getInputNode()->onClickTrackNode(false);
+        input->getInputNode()->m_cursor->setVisible(false);
+    }
+}
---
+++ src/utils/utils.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+class Utils {
+public:
+    /// @brief Get the creation time of the macro file
+    static std::time_t getFileCreationTime(const std::filesystem::path& path);
+
+    /// @brief Formats a time_t as local time (YYYY-MM-DD HH:MM:SS)
+    static std::string formatTime(std::time_t time);
+
+    /// @brief Getter for the background texture
+    static std::string getTexture();
+
+    /// @brief Helper to change the mod's background color
+    static void setBackgroundColor(geode::NineSlice* bg);
+
+    /// @brief Get PauseLayer from the scene
+    static PauseLayer* getPauseLayer();
+
+    /// @brief Sets text input enabled state
+    static void setTextInputEnabled(geode::TextInput* input, 
+        bool enabled, 
+        GLubyte enabledBg = 90,
+        GLubyte disabledBg = 30, 
+        GLubyte enabledText = 255, 
+        GLubyte disabledText = 100);
+};
